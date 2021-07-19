9d03aeff93685035b53355f780275b728b5881cc
unknown <p.abrahao@hotmail.com>
2021-07-19 15:53:47 -0300

    finalizacao RocketQ

--- src/route.js
+++ src/route.js
@@ -10,12 +10,14 @@ const route = express.Router() // Instancia a variavel route com express.Router(
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
+route.post('/create-room', RoomController.create)
+route.get('/room/:room',RoomController.open)
+route.post('/enterroom', RoomController.enter)
+// : pegam as variaveis do html para passagem de informações
 
 // formato que o formulario de dentro da modal deve passar a informação
-     route.post('/question/:room/:question/:action', questionController.index)
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action', questionController.index)
 
-     route.post('/create-room', RoomController.create)
-// : pegam as variaveis do html para passagem de informações
 
 module.exports = route // Exporta a constante route para onde ele é importado, no caso em server.js
